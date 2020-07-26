1fcfe85d55be9b3e9567b97c043faa4ac526a603
gulu2046 <58586162+gulu2046@users.noreply.github.com>
2020-07-26 11:56:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,6 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+Modify default IP
+sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
